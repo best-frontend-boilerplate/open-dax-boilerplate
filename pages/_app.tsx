9bd7e106fd7ae70c02fd34aac46f03b14ad46e6c
gyroflaw <ryo.kanazawa34@gmail.com>
2022-03-11 23:05:25 +0900

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { CoreProvider } from "@openware/opendax-web-sdk";
+import "@openware/opendax-web-sdk/index.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CoreProvider>
+      <Component {...pageProps} />
+    </CoreProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/orderbook.tsx
@@ -0,0 +1,6 @@
+import { OrderBookWidget } from "@openware/opendax-web-sdk";
+
+const OrderBook = () => {
+  return <OrderBookWidget />;
+};
+export default OrderBook;
